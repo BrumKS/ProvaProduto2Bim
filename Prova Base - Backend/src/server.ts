206d7cdf2129997ef6a2e098f72a2f3a7e3e9b2a
BrumKS <brunokauesantos0@gmail.com>
2021-06-21 22:27:42 -0300

    FIM DA PROVA - FRONT E BACKEND

--- Prova Base - Backend/src/server.ts
+++ Prova Base - Backend/src/server.ts
@@ -1,12 +1,14 @@
 console.clear();
 
 import express from "express";
+import cors from "cors";
 import { mongoose } from "./config/database";
 import { router } from "./config/routes";
 
 const app = express();
 const db = mongoose;
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
